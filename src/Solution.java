e8ce3241e353dcb484995f0d6aa045b404df7280
pranithmididoddi <pranith13@gmail.com>
2017-01-04 11:37:05 -0600

    missing number

--- src/Solution.java
+++ src/Solution.java
@@ -54,4 +54,15 @@ public class Solution {
 
         return false;
     }
+
+    public int missingNumber(int[] nums) {
+        int result=0;
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+        }
+
+        int n=nums.length;
+        return n*(n+1)/2-sum;
+    }
 }
